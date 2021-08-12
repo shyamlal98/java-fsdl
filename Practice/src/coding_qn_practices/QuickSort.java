d3dc2206e10bb5188227c029e8d960a8ca545281
shyamlal98 <shyaml.cse16@gmail.com>
2021-08-13 00:52:30 +0530

    QuickSort Implemented

---
+++ Practice/src/coding_qn_practices/QuickSort.java
@@ -0,0 +1,48 @@
+package coding_qn_practices;
+
+public class QuickSort {
+
+	public static void main(String[] args) {
+		int arr[] = {10, 80, 30, 90, 40, 50, 70};
+		quickSort(arr, 0, arr.length-1);
+//		printArray(arr);
+
+	}
+	
+	public static void quickSort(int []arr,int low, int high) {
+		printArray(arr);
+		if(low < high) {
+			int pi = partition(arr,low,high);
+			quickSort(arr,low,pi-1);
+			quickSort(arr,pi+1,high);
+		}
+		
+	}
+	
+	public static int partition(int arr[],int low, int high) {
+		int i=low-1;
+		int pivot = arr[high];
+		for(int j = low; j <= high-1; j++) {
+			if(arr[j] < pivot) {
+				i++;
+				swap(arr,i,j);
+			}
+		}
+		swap(arr,i+1,high);
+		return i+1;
+	}
+	
+	public static void printArray(int arr[]) {
+		for(int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i]+ " ");
+		}
+		System.out.println();
+	}
+	
+	public static void swap(int arr[],int i, int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+
+}
